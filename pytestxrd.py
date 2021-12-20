b29a444ba1106c9a7e98a42355fb0fb78c8a7f73
Anton Schwarz <anton@mendoza-schwarz.de>
2021-12-20 18:51:26 +0100

    initial handshake works

--- pytestxrd.py
+++ pytestxrd.py
@@ -1,2 +1,19 @@
 # Anton Schwarz
 # pytestxrd: conversion of testxrd from perl to python
+
+import socket
+from struct import pack
+from functools import reduce
+
+# hardcoded for now
+HOST = "taylor.fritz.box"
+PORT = 1094
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    initial_handshake = map(lambda x: pack(">I", x), [0, 0, 0, 4, 2012])
+    s.send(reduce(lambda x, y: x + y, initial_handshake))
+    data = s.recv(1024)
+
+print(repr(data))
