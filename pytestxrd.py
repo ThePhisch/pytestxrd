d374f4ffd77204457c1fa6db89de26a592ff1596
Anton Schwarz <anton@mendoza-schwarz.de>
2021-12-22 12:50:08 +0100

    definitions

--- pytestxrd.py
+++ pytestxrd.py
@@ -2,8 +2,11 @@
 # pytestxrd: conversion of testxrd from perl to python
 
 import socket
+import os
 from struct import pack
 from functools import reduce
+
+from definitions import request_codes
 
 # hardcoded for now
 HOST = "taylor.fritz.box"
@@ -11,9 +14,24 @@ PORT = 1094
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-
-    initial_handshake = map(lambda x: pack(">I", x), [0, 0, 0, 4, 2012])
+    # INITIAL HANDSHAKE
+    initial_handshake = map(lambda x: pack("!i", x), [0, 0, 0, 4, 2012])
     s.send(reduce(lambda x, y: x + y, initial_handshake))
     data = s.recv(1024)
+
+    # LOGIN
+    inputs = [
+        pack("!H", request_codes.kXR_login),
+        pack("!i", os.getpid()),
+        pack("!c", b"0"),
+        # pack(">c", os.environ["USER"]),
+        pack("!c", b"0"),  # Ability 0, standard (leave for now)
+        # pack(">c", request_codes.kXR_login | 4),
+        pack("!c", b"0"),
+        pack("!c", b"0"),
+    ]
+
+    s.send(reduce(lambda a, b: a + b, inputs))
+
 
 print(repr(data))
